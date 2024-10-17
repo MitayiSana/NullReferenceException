07422b17ce476d4218314e60a3f46daee31edc28
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:42:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NullReferenceExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s= null;
+            try
+            {
+                if(s == null )
+                {
+                    throw new NullReferenceException();
+                }
+            }
+            catch(NullReferenceException e)
+            {
+                Console.WriteLine("Null Reference Exception");
+            }
+
+           
+        }
+    }
+}
